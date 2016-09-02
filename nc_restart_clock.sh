a5b9aa66b64e0d3599f88b94daae66d9f77b5133
rockysoar <rocky.soar@gmail.com>
2016-09-02 14:21:16 +0800

    加注释

--- nc_restart_clock.sh
+++ nc_restart_clock.sh
@@ -10,6 +10,7 @@ if [ ! -f $RESTART_LOCK ]; then
     return 0
 fi
 
+# 无需重启
 if [ '1' -ne `cat $RESTART_LOCK` ]; then
     return 0
 fi
@@ -18,6 +19,7 @@ fi
 echo >> $RESTART_REASON
 `/etc/init.d/nutcracker restart >> $RESTART_REASON 2>&1`
 
+# 发邮件最好用uuencode处理编码否则body会被当做附件发出；用tr做简单过滤
 tr -t "\r" "\n" < $RESTART_REASON | tr -cd '\11\12\40-\176' | mail -s "Twemproxy restart alarm" $MAIL_ALARM 
 
 #重置锁文件
