ecc64a106aa94bf149fb52c8786e0ec581b19c9b
rockysoar <rocky.soar@gmail.com>
2016-12-22 21:31:37 +0800

    fsdk-redis工具

---
+++ fsdk-redis.php
@@ -0,0 +1,196 @@
+<?php
+/**
+ * Author: 君升 junhua.zhang@fanli.com
+ * Created Time: Fri 25 Nov 2016 09:46:30 PM CST
+ */
+$dsns = [
+    'app' => array(
+        's0000' => array('host' => 'app.redis-s0000.51fanli.it', 'port' => '7001', 'timeout' => 10),
+        's0001' => array('host' => 'app.redis-s0001.51fanli.it', 'port' => '7002', 'timeout' => 10),
+        's0010' => array('host' => 'app.redis-s0010.51fanli.it', 'port' => '7003', 'timeout' => 10),
+        's0011' => array('host' => 'app.redis-s0011.51fanli.it', 'port' => '7004', 'timeout' => 10),
+        's0100' => array('host' => 'app.redis-s0100.51fanli.it', 'port' => '7005', 'timeout' => 10),
+        's0101' => array('host' => 'app.redis-s0101.51fanli.it', 'port' => '7006', 'timeout' => 10),
+        's0110' => array('host' => 'app.redis-s0110.51fanli.it', 'port' => '7007', 'timeout' => 10),
+        's0111' => array('host' => 'app.redis-s0111.51fanli.it', 'port' => '7008', 'timeout' => 10),
+    ),
+    'fsdk' => array(
+        's0' => array('host' => 'zhihui.redis2.51fanli.it', 'port' => '6382', 'timeout' => 10),
+    ),
+    'goshop' => array(
+        's0000' => array('host' => 'goshop.redis-s0000.51fanli.it', 'port' => '6601', 'timeout' => 10),
+        's0001' => array('host' => 'goshop.redis-s0001.51fanli.it', 'port' => '6602', 'timeout' => 10),
+        's0010' => array('host' => 'goshop.redis-s0010.51fanli.it', 'port' => '6603', 'timeout' => 10),
+        's0011' => array('host' => 'goshop.redis-s0011.51fanli.it', 'port' => '6604', 'timeout' => 10),
+        's0100' => array('host' => 'goshop.redis-s0100.51fanli.it', 'port' => '6605', 'timeout' => 10),
+        's0101' => array('host' => 'goshop.redis-s0101.51fanli.it', 'port' => '6606', 'timeout' => 10),
+        's0110' => array('host' => 'goshop.redis-s0110.51fanli.it', 'port' => '6607', 'timeout' => 10),
+        's0111' => array('host' => 'goshop.redis-s0111.51fanli.it', 'port' => '6608', 'timeout' => 10),
+    ),
+    'magicbar' => array(
+        's0' => array('host' => 'magicbar.redis-s0.51fanli.it', 'port' => '6502', 'timeout' => 10),
+    ),
+    'passport' => array(
+        's0000' => array('host' => 'passport.redis-s0000.51fanli.it', 'port' => '6701', 'timeout' => 10),
+        's0001' => array('host' => 'passport.redis-s0001.51fanli.it', 'port' => '6702', 'timeout' => 10),
+        's0010' => array('host' => 'passport.redis-s0010.51fanli.it', 'port' => '6703', 'timeout' => 10),
+        's0011' => array('host' => 'passport.redis-s0011.51fanli.it', 'port' => '6704', 'timeout' => 10),
+        's0100' => array('host' => 'passport.redis-s0100.51fanli.it', 'port' => '6705', 'timeout' => 10),
+        's0101' => array('host' => 'passport.redis-s0101.51fanli.it', 'port' => '6706', 'timeout' => 10),
+        's0110' => array('host' => 'passport.redis-s0110.51fanli.it', 'port' => '6707', 'timeout' => 10),
+        's0111' => array('host' => 'passport.redis-s0111.51fanli.it', 'port' => '6708', 'timeout' => 10),
+    ),
+    'taobao' => array(
+        's0' => array('host' => 'taobao.redis-s0.51fanli.it', 'port' => '6504', 'timeout' => 10),
+    ),
+    'user' => array(
+        's0000' => array('host' => 'user.redis-s0000.51fanli.it', 'port' => '6801', 'timeout' => 10),
+        's0001' => array('host' => 'user.redis-s0001.51fanli.it', 'port' => '6802', 'timeout' => 10),
+        's0010' => array('host' => 'user.redis-s0010.51fanli.it', 'port' => '6803', 'timeout' => 10),
+        's0011' => array('host' => 'user.redis-s0011.51fanli.it', 'port' => '6804', 'timeout' => 10),
+        's0100' => array('host' => 'user.redis-s0100.51fanli.it', 'port' => '6805', 'timeout' => 10),
+        's0101' => array('host' => 'user.redis-s0101.51fanli.it', 'port' => '6806', 'timeout' => 10),
+        's0110' => array('host' => 'user.redis-s0110.51fanli.it', 'port' => '6807', 'timeout' => 10),
+        's0111' => array('host' => 'user.redis-s0111.51fanli.it', 'port' => '6808', 'timeout' => 10),
+    ),
+    'meili' => array(
+        's0' => array('host' => 'meili.redis-s0.51fanli.it', 'port' => '6395', 'timeout' => 10),
+    ),
+    'super' => array(
+        's0000' => array('host' => 'super.redis-s0000.51fanli.it', 'port' => '7101', 'timeout' => 10),
+        's0001' => array('host' => 'super.redis-s0001.51fanli.it', 'port' => '7102', 'timeout' => 10),
+        's0010' => array('host' => 'super.redis-s0010.51fanli.it', 'port' => '7103', 'timeout' => 10),
+        's0011' => array('host' => 'super.redis-s0011.51fanli.it', 'port' => '7104', 'timeout' => 10),
+        's0100' => array('host' => 'super.redis-s0100.51fanli.it', 'port' => '7105', 'timeout' => 10),
+        's0101' => array('host' => 'super.redis-s0101.51fanli.it', 'port' => '7106', 'timeout' => 10),
+        's0110' => array('host' => 'super.redis-s0110.51fanli.it', 'port' => '7107', 'timeout' => 10),
+        's0111' => array('host' => 'super.redis-s0111.51fanli.it', 'port' => '7108', 'timeout' => 10),
+    ),
+    'brand_shop' => array(
+        's0' => array('host' => 'mixed.redis-s0.51fanli.it', 'port' => '6381', 'timeout' => 10),
+    ),
+    'budoumm' => array(
+        's0' => array('host' => 'budoumm.redis-s0.51fanli.it', 'port' => '6388', 'timeout' => 10),
+    ),
+    'tg' => array(
+        's0' => array('host' => 'tuangou.redis1.51fanli.it', 'port' => '6490', 'timeout' => 10),
+    ),
+    'market' => array(
+        's0' => array('host' => 'mixed.redis-s0.51fanli.it', 'port' => '6381', 'timeout' => 10),
+    ),
+    'event' => array(
+        's0' => array('host' => 'data.redis1.51fanli.it', 'port' => '6386', 'timeout' => 10),
+    ),
+    'rule' => array(
+        's0000' => array('host' => 'rule.redis-s0000.51fanli.it', 'port' => '7201', 'timeout' => 10),
+        's0001' => array('host' => 'rule.redis-s0001.51fanli.it', 'port' => '7202', 'timeout' => 10),
+        's0010' => array('host' => 'rule.redis-s0010.51fanli.it', 'port' => '7203', 'timeout' => 10),
+        's0011' => array('host' => 'rule.redis-s0011.51fanli.it', 'port' => '7204', 'timeout' => 10),
+        's0100' => array('host' => 'rule.redis-s0100.51fanli.it', 'port' => '7205', 'timeout' => 10),
+        's0101' => array('host' => 'rule.redis-s0101.51fanli.it', 'port' => '7206', 'timeout' => 10),
+        's0110' => array('host' => 'rule.redis-s0110.51fanli.it', 'port' => '7207', 'timeout' => 10),
+        's0111' => array('host' => 'rule.redis-s0111.51fanli.it', 'port' => '7208', 'timeout' => 10),
+    ),
+    'huodong' => array(
+        's0000' => array('host' => 'huodong.redis-s0000.51fanli.it', 'port' => '7701', 'timeout' => 10),
+        's0001' => array('host' => 'huodong.redis-s0001.51fanli.it', 'port' => '7702', 'timeout' => 10),
+        's0010' => array('host' => 'huodong.redis-s0010.51fanli.it', 'port' => '7703', 'timeout' => 10),
+        's0011' => array('host' => 'huodong.redis-s0011.51fanli.it', 'port' => '7704', 'timeout' => 10),
+        's0100' => array('host' => 'huodong.redis-s0100.51fanli.it', 'port' => '7705', 'timeout' => 10),
+        's0101' => array('host' => 'huodong.redis-s0101.51fanli.it', 'port' => '7706', 'timeout' => 10),
+        's0110' => array('host' => 'huodong.redis-s0110.51fanli.it', 'port' => '7707', 'timeout' => 10),
+        's0111' => array('host' => 'huodong.redis-s0111.51fanli.it', 'port' => '7708', 'timeout' => 10),
+    ),
+    'shop' => array( 
+        's0' => array('host' => 'shop.redis-s0.51fanli.it', 'port' => '7302', 'timeout' => 10),
+    ),
+    'betatest' => array(
+        's0' => array('host' => 'mixed.redis-s0.51fanli.it', 'port' => '6381', 'timeout' => 10),
+    ),
+    'flip_scripts' => array(
+        's0' => array('host' => 'flip.redis.51fanli.it', 'port' => '6394', 'timeout' => 10),
+    ),
+    'mapi' => array(
+        's0000' => array('host' => 'mapi.redis-s0000.51fanli.it', 'port' => '7501', 'timeout' => 10),
+        's0001' => array('host' => 'mapi.redis-s0001.51fanli.it', 'port' => '7502', 'timeout' => 10),
+        's0010' => array('host' => 'mapi.redis-s0010.51fanli.it', 'port' => '7503', 'timeout' => 10),
+        's0011' => array('host' => 'mapi.redis-s0011.51fanli.it', 'port' => '7504', 'timeout' => 10),
+        's0100' => array('host' => 'mapi.redis-s0100.51fanli.it', 'port' => '7505', 'timeout' => 10),
+        's0101' => array('host' => 'mapi.redis-s0101.51fanli.it', 'port' => '7506', 'timeout' => 10),
+        's0110' => array('host' => 'mapi.redis-s0110.51fanli.it', 'port' => '7507', 'timeout' => 10),
+        's0111' => array('host' => 'mapi.redis-s0111.51fanli.it', 'port' => '7508', 'timeout' => 10),
+    ),
+    'hotel' => array(
+        's0' => array('host' => 'hotel.redis-s0.51fanli.it', 'port' => '6493', 'timeout' => 10),
+    ),
+    'movie' => array(
+        's0' => array('host' => 'movie.redis-s0.51fanli.it', 'port' => '6493', 'timeout' => 10),
+    ),
+    'xproject' => array(
+        's0' => array('host' => 'acctsys.redis1.51fanli.it', 'port' => '6379', 'timeout' => 10),
+    ),
+    'fbmall' => array(
+        's0' => array('host' => 'mixed.redis-s0.51fanli.it', 'port' => '6381', 'timeout' => 10),
+    ),
+    'fun' => array(
+        's0' => array('host' => 'mixed.redis-s0.51fanli.it', 'port' => '6381', 'timeout' => 10),
+    ),
+    'gongyi' => array(
+        's0' => array('host' => 'mixed.redis-s0.51fanli.it', 'port' => '6381', 'timeout' => 10),
+    ),
+    'group' => array(
+        's0' => array('host' => 'mixed.redis-s0.51fanli.it', 'port' => '6381', 'timeout' => 10),
+    ),
+    'mixed' => array(
+        's0' => array('host' => 'mixed.redis-s0.51fanli.it', 'port' => '6381', 'timeout' => 10),
+    ),
+    'rbac' => array(
+        's0' => array('host' => 'mixed.redis-s0.51fanli.it', 'port' => '6381', 'timeout' => 10),
+    ),
+    'tests' => array(
+        's0' => array('host' => 'mixed.redis-s0.51fanli.it', 'port' => '6381', 'timeout' => 10),
+    ),
+    'travel' => array(
+        's0' => array('host' => 'mixed.redis-s0.51fanli.it', 'port' => '6381', 'timeout' => 10),
+    ),
+    '500w' => array(
+        's0' => array('host' => 'duobao.redis-s0.51fanli.it', 'port' => '6590', 'timeout' => 10),
+    ),
+    'gaea' => array(
+        's0' => array('host' => 'gaea.redis-s0.51fanli.it', 'port' => '6690', 'timeout' => 10),
+    ),
+    'flip_notify' => array(
+        's0' => array('host' => 'zhihui.redis2.51fanli.it', 'port' => '6382', 'timeout' => 10),
+    ),
+    'session' => array(
+        's0000' => array('host' => 'session.redis-s0000.51fanli.it', 'port' => '6901', 'timeout' => 10),
+        's0001' => array('host' => 'session.redis-s0001.51fanli.it', 'port' => '6902', 'timeout' => 10),
+        's0010' => array('host' => 'session.redis-s0010.51fanli.it', 'port' => '6903', 'timeout' => 10),
+        's0011' => array('host' => 'session.redis-s0011.51fanli.it', 'port' => '6904', 'timeout' => 10),
+        's0100' => array('host' => 'session.redis-s0100.51fanli.it', 'port' => '6905', 'timeout' => 10),
+        's0101' => array('host' => 'session.redis-s0101.51fanli.it', 'port' => '6906', 'timeout' => 10),
+        's0110' => array('host' => 'session.redis-s0110.51fanli.it', 'port' => '6907', 'timeout' => 10),
+        's0111' => array('host' => 'session.redis-s0111.51fanli.it', 'port' => '6908', 'timeout' => 10),
+    ),
+];
+
+include '/usr/local/workdata/release/fsdk/lib/fsdk.init.php';
+include '/usr/local/workdata/release/fsdk/lib/FsdkRal/FsdkRalRedis.class.php';
+
+$dsn = $argv[1];
+$cmd = $argv[2];
+$key = $argv[3];
+$slice = FsdkRalManager::getSlice($dsns[$dsn], $key);
+$conf = $dsns[$dsn][$slice];
+$linkDsn = sprintf('tcp://%s:%s', $conf['host'], $conf['port']);
+
+$params = array_slice($argv, 3);
+$redis = new FsdkRalRedis($linkDsn, 1);
+$resp = call_user_func_array(array($redis, $cmd), $params);
+print_r($resp);
+print_r([
+    'dsn' => $dsn,
+    'slice' => $slice,
+    'conf' => $conf,
+]);
+
