9e97908fcbf10e2e6f223859bec0353deb88a4c2
rockysoar <rocky.soar@gmail.com>
2017-04-10 17:03:03 +0800

    语法检查和BOM头检查

--- svnhooks/inspector_encoding.py
+++ svnhooks/inspector_encoding.py
@@ -3,21 +3,27 @@
 import sys,subprocess
 
 SVNLOOK='/usr/bin/svnlook'
-PHP='/usr/local/bin/php'
+PHPBIN='/usr/bin/php'
 
 # the Changed File
 resp = sys.argv[1]
 cf = sys.argv[2].split(' ')[1]
-cf = resp
 if not cf.endswith('.php'): sys.exit(0);
 
 # php syntax check
 try:
-    exitCode = subprocess.check_output('%s cat %s %s | %s -l | head -2 | tail -1')
-except subprocess.CalledProcessError, e:
-    print e
+    cmd = '%s cat %s %s | %s -l' % (SVNLOOK, resp, cf, PHPBIN)
+    output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+except subprocess.CalledProcessError as e:
+    print(e.output)
     sys.exit(1)
 
 # BOM check
-if "\0xEF\0xBB\0xBF" == open(cf,'rb').read(3):
-    print 'BOM file-header in file %s must be removed.' % (cf)
+cmd = '%s cat %s %s | head -1' % (SVNLOOK, resp, cf)
+firstLine = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+if "\0xEF\0xBB\0xBF" == firstLine[0:3]:
+    print('BOM file-header in file %s must be removed.' % (cf))
+    sys.exit(1)
+
+sys.exit(0)
+
