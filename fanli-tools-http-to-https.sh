996c0548f59f9cec16ac23e9d8966a5d2f0a352a
rockysoar <rocky.soar@gmail.com>
2018-01-10 13:38:56 +0800

    use different Regex from different file

--- fanli-tools-http-to-https.sh
+++ fanli-tools-http-to-https.sh
@@ -14,24 +14,24 @@ fi
 # 匹配：
 # 1：特定标签中的http资源，
 # 2：或ThinkPHP模板写法中加载变量而变量未进行fsdk_fit_scheme处理的资源
-UNSAFE_IMG='<img.+?src=("|\0x27)?(http:|\{\$.+(?<!\|fsdk_fit_scheme)\})'
-UNSAFE_LINK='<link.+?href=("|\0x27)?(http:|\{\$.+(?<!\|fsdk_fit_scheme)\})'
-UNSAFE_SCRIPT='<script.+?src=("|\0x27)?(http:|\{\$.+(?<!\|fsdk_fit_scheme)\})'
-UNSAFE_BACKGROUNd='background(-image)?:.*?url\(("|\0x27)?http:\)'
-UNSAFE_JS_SOURCE='^(?!\s*(\/|\*)).*?("|\0x27)?http:'
+UNSAFE_HTML_IMG='<img.+?src\s*=\s*("|\0x27)?(http:|\{\$.+(?<!\|fsdk_fit_scheme)\})'
+UNSAFE_HTML_LINK='<link.+?href\s*=\s*("|\0x27)?(http:|\{\$.+(?<!\|fsdk_fit_scheme)\})'
+UNSAFE_HTML_SCRIPT='<script.+?src\s*=\s*("|\0x27)?(http:|\{\$.+(?<!\|fsdk_fit_scheme)\})'
+UNSAFE_CSS_BACKGROUND='background(-image)?\s*:.*?url\(("|\0x27)?http:\/\/'
+# match "http://xxx" that not commented out
+UNSAFE_JS_SOURCE='^(?!\s*(\/|\*)).*?("|\0x27)?http:\/\/'
 
 function func_grep () {
-    \grep -nP "$1" \
-        --include=*.php --include=*.htm --include=*.html --include=*.html5 --include=*.css --include=*.js \
-        --exclude-dir=.svn \
-        -r $2
-        #--color=auto \
+   grep -nP "$UNSAFE_HTML_IMG|$UNSAFE_HTML_LINK|$UNSAFE_HTML_SCRIPT" --exclude-dir=.svn --include=*.php -r $1
+   grep -nP "$UNSAFE_CSS_BACKGROUND" --exclude-dir=.svn --include=*.css -r $1
+   grep -nP "$UNSAFE_JS_SOURCE|$UNSAFE_HTML_IMG|$UNSAFE_HTML_LINK|$UNSAFE_HTML_SCRIPT" --exclude-dir=.svn --include=*.js -r $1
+   grep -nP "$UNSAFE_HTML_IMG|$UNSAFE_HTML_LINK|$UNSAFE_HTML_SCRIPT|$UNSAFE_CSS_BACKGROUND|$UNSAFE_JS_SOURCE" --exclude-dir=.svn --include=*.htm --include=*.html --include=*.html5 -r $1
 }
 
 if [[ $OUTPUTSTYLE = "" || $OUTPUTSTYLE = "1" ]]; then
-    func_grep "($UNSAFE_IMG|$UNSAFE_LINK|$UNSAFE_SCRIPT|$UNSAFE_BACKGROUNd|$UNSAFE_JS_SOURCE)" "$PROJ"
+    func_grep "$PROJ"
 elif [[ $OUTPUTSTYLE != "1" ]]; then
-    func_grep "($UNSAFE_IMG|$UNSAFE_LINK|$UNSAFE_SCRIPT|$UNSAFE_BACKGROUNd|$UNSAFE_JS_SOURCE)" "$PROJ" | \
+    func_grep "$PROJ" | \
         gawk -F':' '{
             if(!($1 in arr)) {
                 arr[$1] = 1
