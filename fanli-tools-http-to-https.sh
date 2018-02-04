7a156b0f90366b9d58e87a4eab21930d1d559053
rockysoar <rocky.soar@gmail.com>
2018-02-04 16:40:51 +0800

    bugfix a regex Lookbehind Assertion

--- fanli-tools-http-to-https.sh
+++ fanli-tools-http-to-https.sh
@@ -14,18 +14,18 @@ fi
 # 匹配：
 # 1：特定标签中的http资源，
 # 2：或ThinkPHP模板写法中加载变量而变量未进行fsdk_fit_scheme处理的资源
-UNSAFE_HTML_IMG='<img.+?src\s*=\s*("|\0x27)?(http:|\{\$.+(?<!\|fsdk_fit_scheme)\})'
-UNSAFE_HTML_LINK='<link.+?href\s*=\s*("|\0x27)?(http:|\{\$.+(?<!\|fsdk_fit_scheme)\})'
-UNSAFE_HTML_SCRIPT='<script.+?src\s*=\s*("|\0x27)?(http:|\{\$.+(?<!\|fsdk_fit_scheme)\})'
-UNSAFE_CSS_BACKGROUND='background(-image)?\s*:.*?url\(("|\0x27)?(http:|\{\$.+(?<!\|fsdk_fit_scheme)\})'
+UNSAFE_HTML_IMG='<img.+?src\s*=\s*("|\0x27)?(http:|\{\$[^}]+(?<!\|fsdk_fit_scheme)\})'
+UNSAFE_HTML_LINK='<link.+?href\s*=\s*("|\0x27)?(http:|\{\$[^}]+(?<!\|fsdk_fit_scheme)\})'
+UNSAFE_HTML_SCRIPT='<script.+?src\s*=\s*("|\0x27)?(http:|\{\$[^}]+(?<!\|fsdk_fit_scheme)\})'
+UNSAFE_CSS_BACKGROUND='background(-image)?\s*:.*?url\(("|\0x27)?http:\/\/'
 # match "http://xxx" that not commented out
 UNSAFE_JS_SOURCE='^(?!\s*(\/|\*)).*?("|\0x27)?http:\/\/'
 
 function func_grep () {
    grep -nP "$UNSAFE_HTML_IMG|$UNSAFE_HTML_LINK|$UNSAFE_HTML_SCRIPT" --exclude-dir=.svn --include=*.php -r $1
    grep -nP "$UNSAFE_CSS_BACKGROUND" --exclude-dir=.svn --include=*.css -r $1
-   grep -nP "$UNSAFE_JS_SOURCE|$UNSAFE_HTML_IMG|$UNSAFE_HTML_LINK|$UNSAFE_HTML_SCRIPT" --exclude-dir=.svn --include=*.js -r $1
-   grep -nP "$UNSAFE_HTML_IMG|$UNSAFE_HTML_LINK|$UNSAFE_HTML_SCRIPT|$UNSAFE_CSS_BACKGROUND|$UNSAFE_JS_SOURCE" --exclude-dir=.svn --include=*.htm --include=*.html --include=*.html5 -r $1
+   #grep -nP "$UNSAFE_JS_SOURCE|$UNSAFE_HTML_IMG|$UNSAFE_HTML_LINK|$UNSAFE_HTML_SCRIPT" --exclude-dir=.svn --include=*.js -r $1
+   grep -nP "$UNSAFE_HTML_IMG|$UNSAFE_HTML_LINK|$UNSAFE_HTML_SCRIPT|$UNSAFE_CSS_BACKGROUND" --exclude-dir=.svn --include=*.htm --include=*.html --include=*.html5 -r $1
 }
 
 if [[ $OUTPUTSTYLE = "" || $OUTPUTSTYLE = "1" ]]; then
