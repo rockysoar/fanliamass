3209a28a2be0316b30a2b7d2961ab4a54c8e56b0
junhua.zhang <junhua.zhang@fanli.com>
2019-01-31 13:53:21 +0800

    add lazygit, add git alias

--- centos-bash-functions.sh
+++ centos-bash-functions.sh
@@ -2,8 +2,18 @@
 
 alias ..='cd ..'
 alias ...='cd ../..'
+alias ssh.pub='ssh junhua.zhang@192.168.0.246'
+alias git.lazy="/usr/local/lazygit"
+alias vi.vhost='_(){ VHOSTROOT="/usr/local/apache2/conf/vhosts"; echo "$VHOSTROOT/$1"; usleep 1000000; vim "$VHOSTROOT/${1}_vhost.conf"; }; _'
 
 function g.code {
-    grep -Pn --color=auto --include=*.{sh,py,php,js,css,html,c} --exclude=.{svn,gitignore} "$1" $2 $3
+    grep -Pn --color=auto --include=*.{sh,py,php,js,css,html,c} --exclude=.{svn,gitignore} --exclude-dir=Runtime "$1" $2 $3 $4
 }
+
+if [[ ! -z $(which git) ]]; then
+    git config --global alias.co checkout
+    git config --global alias.st status
+    git config --global alias.ci commit 
+    git config --global alias.di diff
+fi
 
