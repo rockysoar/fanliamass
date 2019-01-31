ba7dd8c8c4d37b8746e12b218ed60b4a7b6ea5e6
junhua.zhang <junhua.zhang@fanli.com>
2019-01-31 13:52:06 +0800

    add pub aliyun based on rsync

---
+++ pub-aliyun.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+ENV="$1"
+PROJS=("${@:1}")
+WEBROOT='/usr/local/webdata'
+SSHPWD=${ALIYUN_SSH_PWD:-''}
+
+COL_RED='\033[0;31m'
+COL_NO='\033[0;0m'
+
+err() {
+    echo -e "[${COL_RED}error${COL_NO}] ${1}"
+    exit 255
+}
+
+log() { 
+    echo -e "[info] ${1}" 
+}
+
+if [[ -z "$SSHPWD" ]]; then
+    err '`export ALIYUN_SSH_PWD="YOUR ALIYUN SSH PASSWORD"` first.'
+fi
+
+IFS=$" \t\n"
+for proj in "${PROJS[@]}"; do
+    log "publishing ${proj}..."
+
+    if [[ ! -d "$WEBROOT/$proj" ]]; then
+        err "project NOT exists: $WEBROOT/$proj."
+    fi
+
+    # copy symlinks as symlinks
+    rsync -av --max-size=2M --delete --delete-during \
+        -e "sshpass -p ${SSHPWD} ssh -o StrictHostKeyChecking=no -l tuangouadmin" \
+        --exclude='.env' --exclude='.idel' --exclude='.git*' --exclude='.svn*' --exclude='Conf' --exclude='config' --exclude='Runtime' \
+        "$WEBROOT/$proj" "tuangouadmin@47.93.253.116:$WEBROOT/$proj"
+
+    log "publish ${proj} complete."
+done
+
